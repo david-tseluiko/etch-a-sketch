7a06176a85d6e21478856ffd4437dfc8d4db40c7
David Tseluiko <david.tseluiko@gmail.com>
2025-09-14 18:54:19 -0500

    Add JS to grid size button and make grid template dynamic

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const container = document.querySelector(".container");
+const gridSizeButton = document.querySelector(".grid-size-btn");
 
 createGrid(16);
 
+gridSizeButton.addEventListener("click", () => {
+    container.textContent = "";
+
+    let gridSize;
+
+    do {
+        gridSize = +prompt("Enter number of squares per side");
+    } while (isNaN(gridSize) || gridSize < 0 || gridSize > 100);
+
+    createGrid(gridSize);
+});
+
 container.addEventListener("mouseover", (event) => {
-    event.target.classList.add('colored');
+    event.target.classList.add("colored");
 });
 
 function createGrid(size) {
@@ -11,6 +24,10 @@ function createGrid(size) {
 
     for (let i = 0; i < gridPixels; i++) {
         const gridElement = document.createElement("div");
+        gridElement.style.cssText = `width: ${700 / size}px; height: ${
+            700 / size
+        }px;`;
+
         container.appendChild(gridElement);
     }
 }
